15fd746d5be90f48d07be8d08e06ff8acbd5e9d5
Diane <dianeyasminec60@gmail.com>
2023-04-03 11:59:00 -0400

    in progress

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Icon } from '@iconify/react';
-// import { Icon } from '@iconify/react';
-// import { Icon } from '@iconify/react';
+
 
 export default function Cards(props) {
   const {
