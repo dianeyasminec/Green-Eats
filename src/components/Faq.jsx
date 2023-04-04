189b1570cc183f49b6228af62caa7cdd9c165b6d
Diane <dianeyasminec60@gmail.com>
2023-04-04 13:07:55 -0400

    Update Faq.jsx

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,33 +1,32 @@
 export default function Faq() {
-    return (
-      <div className="faq text-dark">
-        <h3>Frequently Asked Questions</h3>
-  
-        <div className="question">
-          <p><strong>Lorem ipsum dolor sit amet.</strong></p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, recusandae doloribus qui molestias similique asperiores incidunt. Obcaecati tenetur consectetur dolorem eius ex, ad, laudantium inventore quia odio minus eligendi ipsa?</p>
-        </div>
-  
-        <div className="question">
-          <p><strong>Lorem ipsum dolor sit amet.</strong></p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, recusandae doloribus qui molestias similique asperiores incidunt. Obcaecati tenetur consectetur dolorem eius ex, ad, laudantium inventore quia odio minus eligendi ipsa?</p>
-        </div>
-  
-        <div className="question">
-          <p><strong>Lorem ipsum dolor sit amet.</strong></p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, recusandae doloribus qui molestias similique asperiores incidunt. Obcaecati tenetur consectetur dolorem eius ex, ad, laudantium inventore quia odio minus eligendi ipsa?</p>
-        </div>
-  
-        <div className="question">
-          <p><strong>Lorem ipsum dolor sit amet.</strong></p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, recusandae doloribus qui molestias similique asperiores incidunt. Obcaecati tenetur consectetur dolorem eius ex, ad, laudantium inventore quia odio minus eligendi ipsa?</p>
-        </div>
-  
-        <div className="question">
-          <p><strong>Lorem ipsum dolor sit amet.</strong></p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, recusandae doloribus qui molestias similique asperiores incidunt. Obcaecati tenetur consectetur dolorem eius ex, ad, laudantium inventore quia odio minus eligendi ipsa?</p>
-        </div>
-  
+  return (
+    <div className="faq text-dark">
+      <h3>Frequently Asked Questions</h3>
+
+      <div className="question">
+        <p><strong>What forms of payment do you accept?</strong></p>
+        <p>We accept all major credit cards, as well as PayPal and Apple Pay.</p>
       </div>
-    )
-  }
+
+      <div className="question">
+        <p><strong>How long does shipping take?</strong></p>
+        <p>Shipping times vary depending on your location and the shipping method you choose at checkout. We offer standard and expedited shipping options.</p>
+      </div>
+
+      <div className="question">
+        <p><strong>What is your return policy?</strong></p>
+        <p>We offer a 30-day return policy for most items. Please see our Returns page for more information.</p>
+      </div>
+
+      <div className="question">
+        <p><strong>Can I track my order?</strong></p>
+        <p>Yes, you will receive a tracking number once your order has shipped.</p>
+      </div>
+
+      <div className="question">
+        <p><strong>Do you offer international shipping?</strong></p>
+        <p>Yes, we offer international shipping to select countries. Please see our Shipping page for more information.</p>
+      </div>
+    </div>
+  )
+}
