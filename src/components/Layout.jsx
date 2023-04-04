b3f581d0920b8bc5f6c1028e94da211ba4e4436d
Diane <dianeyasminec60@gmail.com>
2023-04-04 09:08:02 -0400

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
-
-
-
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Layout() {
   return (
-
-
-
     <div className='root-layout'>
-    <header>
-      <nav>
-      <Navbar.Brand href="#home" >
-        </Navbar.Brand>
-        <NavLink to="/">About</NavLink>
-        <NavLink to="allrecipes">All Recipes</NavLink>
-        <NavLink to="reviews">Reviews</NavLink>
-        <NavLink to="help">Help</NavLink>
-      </nav>
-    </header>
-    <main>
-    <Outlet/>
-    </main>
-      </div>
+      <header>
+        <nav>
+          <Navbar.Brand href="#home" >
+          </Navbar.Brand>
+          <NavLink to="/">About</NavLink>
+          <NavLink to="allrecipes">All Recipes</NavLink>
+          <NavLink to="reviews">Reviews</NavLink>
+          <NavDropdown title="Help">
+            <NavDropdown.Item to="faq">FAQ</NavDropdown.Item>
+            <NavDropdown.Item href="contact">Contact Us</NavDropdown.Item>
+          </NavDropdown>
+        </nav>
+      </header>
+      <main>
+        <Outlet/>
+      </main>
+    </div>
   )
 }
+
