6342b29aae4f1be4a4c390f8057ba4c4a0a6431d
Diane <dianeyasminec60@gmail.com>
2023-04-04 13:03:28 -0400

    Update HelpLayout.jsx

--- src/components/HelpLayout.jsx
+++ src/components/HelpLayout.jsx
@@ -3,11 +3,14 @@ import { Outlet, NavLink } from 'react-router-dom'
 
 export default function HelpLayout() {
   return (
-    <div className='help-layout'>
-        <h2>Website Help</h2>
-        {/* lorem10 */}
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, debitis?</p>
-
+    <div className='help-layout text-center'>
+        <h2 >Website Help</h2>
+         <p>
+         Welcome to our Help section! Here you can find answers to frequently asked questions 
+         and get in touch with our support team through our Contact page. If you have any 
+         questions or concerns about our product or service, please don't hesitate to reach out.
+          Our friendly support team is always ready to assist you
+          </p>
         <nav>
         <NavLink to="faq">View the FAQ</NavLink>
         <NavLink to="contact">Contact Us</NavLink>
