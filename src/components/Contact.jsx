f5329a0e56492f4ef80865e6f50bcc4e1609b050
Diane <dianeyasminec60@gmail.com>
2023-04-04 12:46:38 -0400

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,27 @@
 export default function Contact() {
-    return (
-      <div className="contact text-dark">
-        <h3>Contact Us</h3>
-        <form>
-          <label>
-            <span>Your email:</span>
-            <input type="email" name="email" required />
-          </label>
-          <label>
-            <span>Your message:</span>
-            <textarea name="message" required></textarea>
-          </label>
-          <button>Submit</button>
-        </form>
-      </div>
-    )
-  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    console.log({
+      email: formData.get("email"),
+      message: formData.get("message"),
+    });
+  };
+
+  return (
+    <div className="contact text-dark">
+      <h3>Contact Us</h3>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Your email:</span>
+          <input type="email" name="email" required className="form-input" />
+        </label>
+        <label>
+          <span>Your message:</span>
+          <textarea name="message" required className="form-input"></textarea>
+        </label>
+        <button className="form-button">Submit</button>
+      </form>
+    </div>
+  );
+}
