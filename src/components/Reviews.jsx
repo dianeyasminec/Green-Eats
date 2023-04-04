10a9363eef3caf95df0e3059708e284fba1548de
Diane <dianeyasminec60@gmail.com>
2023-04-04 00:03:13 -0400

    update

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+// import React from 'react'
 
-export default function Reviews() {
+// export default function Reviews() {
+//   return (
+//     <div>Reviews</div>
+//   )
+// }
+
+import React, { useState } from 'react';
+
+const Reviews = () => {
+  const [reviews, setReviews] = useState([
+    { id: 1, user: 'John', rating: 5, comment: 'This recipe is amazing!' },
+    { id: 2, user: 'Jane', rating: 4, comment: 'It was pretty good.' },
+    { id: 3, user: 'Bob', rating: 3, comment: 'It was just okay.' },
+  ]);
+
+  const renderStars = (rating) => {
+    let stars = '';
+    for (let i = 0; i < rating; i++) {
+      stars += '★';
+    }
+    for (let i = rating; i < 5; i++) {
+      stars += '☆';
+    }
+    return stars;
+  };
+
   return (
-    <div>Reviews</div>
-  )
-}
+    <div>
+      {reviews.map((review) => (
+        <div key={review.id}>
+          <h3>{review.user}</h3>
+          <p>{renderStars(review.rating)}</p>
+          <p>{review.comment}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Reviews;
