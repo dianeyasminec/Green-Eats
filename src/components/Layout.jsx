fbb4582d8f9f7477c52efed7dfc2adc8ff65eb76
Diane <dianeyasminec60@gmail.com>
2023-04-04 13:18:55 -0400

    Done

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
+import {Nav} from 'react-bootstrap'
 
 
 
@@ -13,12 +14,20 @@ export default function Layout() {
     <div className='root-layout'>
     <header>
       <nav>
-      <Navbar.Brand href="#home" >
-        </Navbar.Brand>
+      <Navbar.Brand > 
+            Nourish & Nature
+      </Navbar.Brand>
+
+      <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
         <NavLink to="/">About</NavLink>
         <NavLink to="allrecipes">All Recipes</NavLink>
         <NavLink to="reviews">Reviews</NavLink>
         <NavLink to="help">Help</NavLink>
+        </Nav>
       </nav>
     </header>
     <main>
