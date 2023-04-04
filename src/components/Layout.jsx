c7aac068dddd5ac6444ea6d34c019bb7f1375b53
Diane <dianeyasminec60@gmail.com>
2023-04-04 13:31:29 -0400

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,36 +3,27 @@ import { NavLink, Outlet } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import {Nav} from 'react-bootstrap'
 
-
-
-
 export default function Layout() {
   return (
-
-
-
     <div className='root-layout'>
-    <header>
-      <nav>
-      <Navbar.Brand > 
-            Nourish & Nature
-      </Navbar.Brand>
-
-      <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: '100px' }}
-              navbarScroll
-            >
-        <NavLink to="/">About</NavLink>
-        <NavLink to="allrecipes">All Recipes</NavLink>
-        <NavLink to="reviews">Reviews</NavLink>
-        <NavLink to="help">Help</NavLink>
-        </Nav>
-      </nav>
-    </header>
-    <main>
-    <Outlet/>
-    </main>
-      </div>
+      <header>
+        <nav>
+          <Navbar.Brand>Green Eats</Navbar.Brand>
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '150px' }}
+            navbarScroll
+          >
+            <NavLink to="/" style={{ marginRight: '150px' }}>About</NavLink>
+            <NavLink to="allrecipes" style={{ marginRight: '150px' }}>All Recipes</NavLink>
+            <NavLink to="reviews" style={{ marginRight: '150px' }}>Reviews</NavLink>
+            <NavLink to="help" style={{ marginRight: '20px' }}>Help</NavLink>
+          </Nav>
+        </nav>
+      </header>
+      <main>
+        <Outlet/>
+      </main>
+    </div>
   )
 }
